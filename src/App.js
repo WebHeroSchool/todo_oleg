bfab3e702bc808fa653351a7cc939766a636a06b
Oleg <>
2020-12-22 21:01:33 +0300

    delete others files

--- src/App.js
+++ src/App.js
@@ -1,36 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-import {count, lenght} from './numbers.js';
+import React from 'react';
+
+const element = (<div>
+    Hello World! Hi!
+  </div>);
 
 
-const number = 3;
-const flag = true;
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p style={{
-          color: 'red'
-        }}>Hello world</p>
-        <p>{number + number}</p>
-        <p>{flag ? 'Is TRUE' : 'is FALSE'}</p>
-        <p>{count * lenght}</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export {element};
