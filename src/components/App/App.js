5488145b43805ed598e99897595ea625e014d3c4
Oleg <>
2020-12-25 15:12:25 +0300

    + React.Component.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,30 +5,33 @@ import InputItem from '../InputItem/Inputitem';
 import styles from './App.module.css';
 
 const todoItem = 'Создать новое приложение';
-const App =()=> {
-  const items = [
-    {
-      value: 'Написать приложение',
-      isDone: true
-    },
-    {
-      value: 'прописать пропсв',
-      isDone: true
-    },
-    {
-      value: 'сделать все дела',
-      isDone: false
-    }
-  ];
 
-  return (
-  <div className={styles.wrap}>
-    <h1 className={styles.title}>Важные дела:</h1>
-    <InputItem />
-    <ItemList items = {items}/>
-    <hr />
-    <Footer count={3} />
-  </div>);
+class App extends React.Component {
+  render () {
+    const items = [
+      {
+        value: 'Написать приложение',
+        isDone: true
+      },
+      {
+        value: 'прописать пропсв',
+        isDone: true
+      },
+      {
+        value: 'сделать все дела',
+        isDone: false
+      }
+    ];
+
+    return (
+    <div className={styles.wrap}>
+      <h1 className={styles.title}>Важные дела:</h1>
+      <InputItem />
+      <ItemList items = {items}/>
+      <hr />
+      <Footer count={3} />
+    </div>);
+  }
 }
 
 export default App;
