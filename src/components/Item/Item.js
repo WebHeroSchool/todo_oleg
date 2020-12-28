b68feef5bfde8e73b5bde2b7fa0e2b7fda354f90
Oleg <>
2020-12-28 14:30:39 +0300

    + Методы жизненного цикла

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -2,13 +2,33 @@ import React from 'react';
 import classnames from 'classnames';
 import styles from '../App/App.module.css';
 
-const Item =({value, isDone, onClickDone, onClickDelete, id} )=>(
-  <div>
-  <span onClick={()=>onClickDone(id)}  className={classnames({[styles.item]: true, [styles.done]: isDone})}>
-  {value }
-  </span>
-  <span onClick={()=>onClickDelete(id)} className={styles.delete}>  X  </span>
-  </div>
-);
+
+class Item extends React.Component {
+  componentDidMount (){
+    console.log('componentDidMount');
+  };
+
+  componentDidUpdate (){
+      console.log('componentDidUpdate');
+  }
+
+  componentWillUnmount (){
+      console.log('componentWillUnmount');
+  }
+
+  render () {
+    const {value, isDone, onClickDone, onClickDelete, id} = this.props;
+    return (
+      <div>
+      <span onClick={()=>onClickDone(id)}  className={classnames({[styles.item]: true, [styles.done]: isDone})}>
+      {value }
+      </span>
+      <span onClick={()=>onClickDelete(id)} className={styles.delete}>  X  </span>
+      </div>
+    )
+  }
+}
+
+
 
 export default Item;
