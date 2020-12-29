f86b1ca077725f7e21d3a3984d7985f9a2362fb3
Oleg <>
2020-12-29 18:30:41 +0300

    + Router

---
+++ src/components/About/about.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from '../App/App.module.css';
+
+class About extends React.Component {
+	render() {
+		return (
+			<h1 className = {styles.wrap}>About</h1>
+		)
+	}
+};
+
+
+export default About;
