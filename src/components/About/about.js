0ed94a5bf686ce9b604ec64cffd915da3a5b58d5
Oleg <>
2021-01-04 14:54:47 +0300

    +

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -2,67 +2,78 @@ import React from 'react';
 import styles from '../App/App.module.css';
 import LinearProgress from '@material-ui/core/LinearProgress';
 import {Octokit} from '@octokit/rest';
+import classnames from "classnames";
 
 const octokit = new Octokit();
 
 class About extends React.Component {
   state = {
+    username: 'StrigunovOleg',
     isLoading: true,
-    repolList: [],
-    nameUser: [],
-    infoUser: [],
-    avatarUser: [],
-    isError: false,
-    error: {}
+    repoList: [],
+    userInfo: [],
+    incorrectRequest: false,
   }
 
   componentDidMount() {
-    octokit.repos.listForUser({
-      username: 'StrigunovOleg'
-    }).then(({ data }) => {
-      this.setState({
-        repolList: data,
-        isLoading: false
-      });
-    }).catch(error => {
-      this.setState({
-        isError: true,
-        error: error
-      })
-    });
 
+    const {username} = this.state;
+
+    octokit.repos.listForUser({
+      username,
+    }).then(({data}) => {
+      this.setState({
+        isLoading: false,
+        repoList: data,
+      });
+    }).catch(() => {
+      this.setState({
+        incorrectRequest: true,
+      });
+    });
 
     octokit.users.getByUsername({
-      username: 'StrigunovOleg'
-    }).then((response) => {
+      username,
+    }).then(({data}) => {
       this.setState({
-        nameUser: response.data.login,
-        avatarUser: response.data.avatar_url,
-        infoUser: response.data.bio,
-        isLoading: false
-      })
-    }).catch(error => {
+        isLoading: false,
+        userInfo: data,
+      });
+    }).catch(() => {
       this.setState({
-        isError: true,
-        error: error
-      })
+        incorrectRequest: true,
+      });
     });
+
   }
 
-  render () {
-    const { isLoading, repolList, nameUser, infoUser, avatarUser } = this.state;
+  render() {
+    const { isLoading, repoList, userInfo, incorrectRequest } = this.state;
+
     return (
-        <div className={styles.wrap}>
-          <h2 className={styles.name}>My name is { nameUser }</h2>
-          <p>{ infoUser }</p>
-          <img className={styles.img} src={avatarUser} alt={nameUser} />
-          <h1 className={styles.title}>{ isLoading ?  <LinearProgress /> : 'My  repositories'}</h1>
-          {!isLoading && <ol>
-            {repolList.map(repo => (<li className={styles.list} key={repo.id}>
-              {repo.name}
-              <a className={styles.link} href={repo.html_url}>Link to GitHub</a>
-            </li>))}
-          </ol>}
+        <div
+            className={classnames({
+              [styles.wrap]: true,
+              [styles.error]: isLoading,
+            })}
+        >
+          {isLoading ? <LinearProgress /> : <div>
+            <h1>Обо мне</h1>
+            <div className={styles.img__wrap}>
+              <img className={styles.avatar} src={userInfo.avatar_url} alt='Avatar'/>
+              <div className={styles.login}> Login to GitHub: <span className={styles.text}>{userInfo.login}</span></div>
+            </div>
+            <p> My name is <span className={styles.text}>{userInfo.name}</span></p>
+            <p> {userInfo.bio}</p>
+            <p>My repositories:</p>
+            <ol className={styles.list}>
+              { repoList.map( (repo) => (
+                  <li className={styles.item} key={repo.id}>
+                    <a className={styles.link} href={repo.html_url}>{repo.name}</a>
+                  </li>))}
+            </ol></div>
+          }
+          {incorrectRequest && <div>Ошибка запроса!</div>}
         </div>
     );
   }
